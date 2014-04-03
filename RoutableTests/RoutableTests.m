clayallsopp/routable-ios

commit 56c1c0d089610bf62b40f9d9807569e162ba3888

Use -[NSString pathComponents] to deal with leading and trailing slashes.

Routable/Routable.m
@@ -253,12 +253,12 @@
     return [self.cachedRoutes objectForKey:url];
   }
   
-  NSArray *givenParts = [url componentsSeparatedByString:@"/"];
+  NSArray *givenParts = url.pathComponents;
   
   RouterParams *openParams = nil;
   for (NSString *routerUrl in self.routes.allKeys) {
     UPRouterOptions *routerOptions = (UPRouterOptions *)[self.routes objectForKey:routerUrl];
-    NSArray *routerParts = [routerUrl componentsSeparatedByString:@"/"];
+    NSArray *routerParts = routerUrl.pathComponents;
     
     if (routerParts.count != givenParts.count) {
       continue;

commit d12d0284171961f2702cc8436851f50f5e9d3867

Fix and test a regression in 56c1c0d089610bf62b40f9d9807569e162ba3888; closes #22

Routable/Routable.m
@@ -259,6 +259,11 @@
   }
   
   NSArray *givenParts = url.pathComponents;
+  NSArray *legacyParts = [url componentsSeparatedByString:@"/"];
+  if ([legacyParts count] != [givenParts count]) {
+    NSLog(@"Routable Warning - your URL %@ has empty path components - this will throw an error in an upcoming release", url);
+    givenParts = legacyParts;
+  }
   
   RouterParams *openParams = nil;
   for (NSString *routerUrl in self.routes.allKeys) {

RoutableTests/RoutableTests.m
@@ -12,6 +12,7 @@
 @interface UserController : UIViewController
 
 @property (readwrite, nonatomic, copy) NSString *userId;
+@property (readwrite, nonatomic, copy) NSString *userName;
 
 - (id)initWithRouterParams:(NSDictionary *)params;
 @end
@@ -21,6 +22,7 @@
 - (id)initWithRouterParams:(NSDictionary *)params {
   if ((self = [self initWithNibName:nil bundle:nil])) {
     self.userId = [params objectForKey:@"user_id"];
+    self.userName = [params objectForKey:@"user_name"];
   }
   return self;
 }
@@ -50,6 +52,7 @@
 @end
 
 #define USER_ID [(UserController *)self.router.navigationController.topViewController userId]
+#define USER_NAME [(UserController *)self.router.navigationController.topViewController userName]
 #define STATIC_USER_ID [(StaticController *)self.router.navigationController.topViewController userId]
 
 @implementation RoutableTests
@@ -76,6 +79,28 @@
   [self.router open:@"users/4"];
   
   STAssertTrue([USER_ID isEqualToString:@"4"], @"Should have an ID of 4");
+}
+
+- (void)test_basicRouteWithMultipleComponents {
+    [self.router map:@"users/:user_id/:user_name" toController:[UserController class]];
+    
+    [self.router open:@"users/4/clay"];
+    
+    STAssertTrue([USER_ID isEqualToString:@"4"], @"Should have an ID of 4");
+    STAssertTrue([USER_NAME isEqualToString:@"clay"], @"Name should be Clay");
+}
+
+- (void)test_basicRouteWithEmptyComponents {
+    [self.router map:@"users/:user_id/:user_name" toController:[UserController class]];
+    
+    [self.router open:@"users//clay"];
+    
+    STAssertTrue([USER_ID isEqualToString:@""], @"Should have an empty ID");
+    STAssertTrue([USER_NAME isEqualToString:@"clay"], @"Name should be Clay");
+
+    [self.router open:@"users/ /clay"];
+    STAssertTrue([USER_ID isEqualToString:@" "], @"Should have an empty ID");
+    STAssertTrue([USER_NAME isEqualToString:@"clay"], @"Name should be Clay");
 }
 
 - (void)test_emptyRoute {