9d7d1dbc207b445ca5a5ba8c5a1fdbbd945e6381
Matthew Healy <matthew@wetheplural.com>
2015-04-18 23:22:16 +0100

    Add tests for NorthStar's new functionality

--- RoutableTests/RoutableTests.m
+++ RoutableTests/RoutableTests.m
@@ -9,10 +9,33 @@
 #import "RoutableTests.h"
 #import "Routable.h"
 
+@interface ExtraParamObject : NSObject
+
+@property NSInteger number;
+@property NSString *string;
+
+- (id)initWithNumber:(NSInteger)number andString:(NSString *)string;
+
+@end
+
+@implementation ExtraParamObject
+
+- (id)initWithNumber:(NSInteger)number andString:(NSString *)string {
+  self = [super init];
+  self.number = number;
+  self.string = string;
+  
+  return self;
+}
+
+@end
+
 @interface UserController : UIViewController
 
 @property (readwrite, nonatomic, copy) NSString *userId;
 @property (readwrite, nonatomic, copy) NSString *userName;
+@property (readwrite, nonatomic, copy) NSString *extraParam;
+@property (readwrite, nonatomic) ExtraParamObject *epObject;
 
 - (id)initWithRouterParams:(NSDictionary *)params;
 @end
@@ -23,6 +46,8 @@
   if ((self = [self initWithNibName:nil bundle:nil])) {
     self.userId = [params objectForKey:@"user_id"];
     self.userName = [params objectForKey:@"user_name"];
+    self.extraParam = [params objectForKey:@"keyOne"];
+    self.epObject = [params objectForKey:@"epObject"];
   }
   return self;
 }
@@ -41,7 +66,7 @@
 + (id)allocWithRouterParams:(NSDictionary *)params {
   StaticController *instance = [[StaticController alloc] initWithNibName:nil bundle:nil];
   instance.userId = [params objectForKey:@"user_id"];
-
+  
   return instance;
 }
 
@@ -54,13 +79,15 @@
 #define USER_ID [(UserController *)self.router.navigationController.topViewController userId]
 #define USER_NAME [(UserController *)self.router.navigationController.topViewController userName]
 #define STATIC_USER_ID [(StaticController *)self.router.navigationController.topViewController userId]
+#define USER_EXTRA_PARAM [(UserController *)self.router.navigationController.topViewController extraParam]
+#define USER_EPOBJECT [(UserController *)self.router.navigationController.topViewController epObject]
 
 @implementation RoutableTests
 @synthesize router = _router;
 
 - (void)setUp {
   [super setUp];
-    
+  
   self.router = [Routable newRouter];
   self.router.navigationController = [[UINavigationController alloc] initWithNibName: nil bundle: nil];
 }
@@ -69,7 +96,7 @@
   // Tear-down code here.
   [self.router.navigationController setViewControllers:[NSArray array]];
   self.router = nil;
-    
+  
   [super tearDown];
 }
 
@@ -82,25 +109,25 @@
 }
 
 - (void)test_basicRouteWithMultipleComponents {
-    [self.router map:@"users/:user_id/:user_name" toController:[UserController class]];
-    
-    [self.router open:@"users/4/clay"];
-    
-    XCTAssertTrue([USER_ID isEqualToString:@"4"], @"Should have an ID of 4");
-    XCTAssertTrue([USER_NAME isEqualToString:@"clay"], @"Name should be Clay");
+  [self.router map:@"users/:user_id/:user_name" toController:[UserController class]];
+  
+  [self.router open:@"users/4/clay"];
+  
+  XCTAssertTrue([USER_ID isEqualToString:@"4"], @"Should have an ID of 4");
+  XCTAssertTrue([USER_NAME isEqualToString:@"clay"], @"Name should be Clay");
 }
 
 - (void)test_basicRouteWithEmptyComponents {
-    [self.router map:@"users/:user_id/:user_name" toController:[UserController class]];
-    
-    [self.router open:@"users//clay"];
-    
-    XCTAssertTrue([USER_ID isEqualToString:@""], @"Should have an empty ID");
-    XCTAssertTrue([USER_NAME isEqualToString:@"clay"], @"Name should be Clay");
-
-    [self.router open:@"users/ /clay"];
-    XCTAssertTrue([USER_ID isEqualToString:@" "], @"Should have an empty ID");
-    XCTAssertTrue([USER_NAME isEqualToString:@"clay"], @"Name should be Clay");
+  [self.router map:@"users/:user_id/:user_name" toController:[UserController class]];
+  
+  [self.router open:@"users//clay"];
+  
+  XCTAssertTrue([USER_ID isEqualToString:@""], @"Should have an empty ID");
+  XCTAssertTrue([USER_NAME isEqualToString:@"clay"], @"Name should be Clay");
+  
+  [self.router open:@"users/ /clay"];
+  XCTAssertTrue([USER_ID isEqualToString:@" "], @"Should have an empty ID");
+  XCTAssertTrue([USER_NAME isEqualToString:@"clay"], @"Name should be Clay");
 }
 
 - (void)test_emptyRoute {
@@ -117,7 +144,7 @@
 
 - (void)test_noNavigationController {
   [self.router map:@"users" toController:[UserController class]];
-
+  
   self.router.navigationController = nil;
   XCTAssertThrows([self.router open:@"users"], @"Should throw an exception when there is no navigation controller");
 }
@@ -159,13 +186,13 @@
   XCTAssertTrue([userId isEqualToString:@"4"], @"Callback should have correct id param; was %@", userId);
 }
 
-- (void)test_staticRoute {  
+- (void)test_staticRoute {
   [self.router map:@"users/:user_id" toController:[StaticController class]];
   
   [self.router open:@"users/4"];
   
   XCTAssertTrue([self.router.navigationController.topViewController isKindOfClass:[StaticController class]], @"Should be an instance of StaticController");
-  XCTAssertTrue([STATIC_USER_ID isEqualToString:@"4"], @"Should have an ID of 4");  
+  XCTAssertTrue([STATIC_USER_ID isEqualToString:@"4"], @"Should have an ID of 4");
 }
 
 - (void)test_noInitializers {
@@ -175,12 +202,95 @@
 }
 
 - (void)test_openAsRoot {
-    [self.router map:@"users/:user_id" toController:[UserController class] withOptions:[UPRouterOptions root]];
-    
-    [self.router open:@"users/4"];
-    
-    XCTAssertTrue([self.router.navigationController.viewControllers[0] isKindOfClass:[UserController class]], @"Should be an instance of UserController");
-    XCTAssertTrue(self.router.navigationController.viewControllers.count == 1, @"Navigation controller should only have 1 view controller in its stack");
+  [self.router map:@"users/:user_id" toController:[UserController class] withOptions:[UPRouterOptions root]];
+  
+  [self.router open:@"users/4"];
+  
+  XCTAssertTrue([self.router.navigationController.viewControllers[0] isKindOfClass:[UserController class]], @"Should be an instance of UserController");
+  XCTAssertTrue(self.router.navigationController.viewControllers.count == 1, @"Navigation controller should only have 1 view controller in its stack");
 }
+
+- (void)test_replaceOldParamWithNew {
+  [self.router map:@"users/:user_id" toController:[UserController class]];
+  
+  [self.router open:@"users/4"];
+  
+  XCTAssertTrue([USER_ID isEqualToString:@"4"]);
+  
+  [self.router open:@"users/3"];
+  
+  XCTAssertTrue([USER_ID isEqualToString:@"3"], @"Should have param 3, was %@", USER_ID);
+  
+}
+
+- (void)test_openWithExtraStringParam {
+  [self.router map:@"users" toController:[UserController class]];
+  
+  NSDictionary *params = [[NSDictionary alloc] initWithObjects:@[@"objOne"] forKeys:@[@"keyOne"]];
+  
+  [self.router open:@"users" animated:NO extraParams:params];
+  
+  XCTAssertEqual(USER_EXTRA_PARAM, @"objOne", @"Should be able to get extra string param in view controller");
+}
+
+- (void)test_openWithExtraObjectParam {
+  [self.router map:@"users" toController:[UserController class]];
+  
+  ExtraParamObject *epObject = [[ExtraParamObject alloc] initWithNumber:99 andString:@"secretString"];
+  NSDictionary *params = [NSDictionary dictionaryWithObjects:@[epObject] forKeys:@[@"epObject"]];
+  
+  [self.router open:@"users" animated:NO extraParams:params];
+  
+  XCTAssertEqual((ExtraParamObject *)USER_EPOBJECT, epObject, @"Should be able to get extra param object in view controller");
+  
+}
+
+- (void)test_openWithURLParamsAndExtraParams {
+  [self.router map:@"users/:user_id" toController:[UserController class]];
+  
+  ExtraParamObject *epObject = [[ExtraParamObject alloc] initWithNumber:99 andString:@"secretString"];
+  NSDictionary *params = [NSDictionary dictionaryWithObjects:@[epObject] forKeys:@[@"epObject"]];
+  
+  [self.router open:@"users/4" animated:NO extraParams:params];
+  
+  XCTAssertTrue([USER_ID isEqualToString:@"4"], @"Should have ID of 4");
+  XCTAssertEqual((ExtraParamObject *)USER_EPOBJECT, epObject, @"Should be able to get extra param object in view controller");
+}
+
+- (void)test_openWithNilExtraParams {
+  [self.router map:@"users" toController:[UserController class]];
+  
+  [self.router open:@"users" animated:NO extraParams:nil];
+  
+  XCTAssertNil(USER_EXTRA_PARAM, @"No extra params should be passed if none are specified");
+}
+
+- (void)test_replaceOldExtraParamWithNew {
+  [self.router map:@"users" toController:[UserController class]];
+  
+  ExtraParamObject *epObject = [[ExtraParamObject alloc] initWithNumber:99 andString:@"secretString"];
+  NSDictionary *params = [NSDictionary dictionaryWithObjects:@[epObject] forKeys:@[@"epObject"]];
+  
+  [self.router open:@"users" animated:NO extraParams:params];
+  
+  ExtraParamObject *receivedObject = (ExtraParamObject *)USER_EPOBJECT;
+  
+  XCTAssertEqual(receivedObject.number, epObject.number);
+  XCTAssertEqual(receivedObject.string, epObject.string);
+  
+  [self.router pop];
+  
+  ExtraParamObject *secondEpObect = [[ExtraParamObject alloc] initWithNumber:1 andString:@"secretString1"];
+  NSDictionary *paramsTwo = [NSDictionary dictionaryWithObjects:@[secondEpObect] forKeys:@[@"epObject"]];
+  
+  [self.router open:@"users" animated:NO extraParams:paramsTwo];
+  
+  ExtraParamObject *secondReceivedObject = (ExtraParamObject *)USER_EPOBJECT;
+  
+  XCTAssertEqual(secondReceivedObject.number, secondEpObect.number);
+  XCTAssertEqual(secondReceivedObject.string, secondEpObect.string);
+  
+}
+
 
 @end
